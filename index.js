92ce0a9208e4e0c0147f0660174677274e2bfd75
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-01-18 00:49:10 +0100

    Done with index router

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { projects } = require('./data.json');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.use('/static', express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index', { projects });
+})
+
+
+
+
+app.listen(3000, () => console.log('Application running on port localhost:3000'));
+
