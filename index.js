3b869981faeac8886090aab1927dc574f9d857c0
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-01-19 01:09:06 +0100

    Done with routes. Handling errors now

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-
+// Routes
 app.get('/', (req, res) => {
     res.render('index', { projects });
 })
@@ -15,8 +15,28 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+app.get('/project/:id', (req, res) => {
+    const { id } = req.params;
+    const project = projects.find(project => project.id === +id);
+    res.render('project', { project })
+});
 
 
+// Handling errors
+app.use((req, res, next) => {
+    const err = new Error("Page doesn't exist. Please try again.");
+    err.status = 404;
+    next(err);
+});
 
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        console.log(err.message);
+    }
+    next(); //handling the 404 error (successfully)
+});
+
+
+// Start server
 app.listen(3000, () => console.log('Application running on port localhost:3000'));
 
